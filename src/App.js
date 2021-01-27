c1581191f742d5a910e9c00f9bd09948257ff5a0
Nick Allen <nwestallen@gmail.com>
2021-01-27 16:14:00 -0500

    controlled form inputs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  
   return (
     <div className="App">
       <h1>Hi friend</h1>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,45 @@
 //Form component
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+
+const UserForm = styled.form`
+display: flex;
+flex-flow: column nowrap;
+width: 300px;
+margin: auto;
+align-items: center;
+border: 2px solid black;
+padding: 40px 0;
+
+button {
+    width: 200px;
+    margin-top: 10px;
+}
+`;
 
 const Form = (props) => {
 
-    const UserForm = styled.form`
-        display: flex;
-        flex-flow: column nowrap;
-        width: 300px;
-        margin: auto;
-        align-items: center;
-        border: 2px solid black;
-        padding: 40px 0;
+    const { users, setUsers } = props;
+    const [ newUser, setNewUser ] = useState({name: '', email: '', acceptedTerms:false});
 
-        button {
-            width: 200px;
-            margin-top: 10px;
-        }
-    `;
+    const handleChange = event => {
+        const update = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+        setNewUser({...newUser, [event.target.name]: update});
+    }
 
     return(
         <UserForm>
             <p>
-                <label for='nameInput'>Name: </label>
-                <input id='nameInput' type='text' />
+                <label htmlFor='nameInput'>Name: </label>
+                <input id='nameInput' type='text' name='name' value={newUser.name} onChange={handleChange}  />
             </p>
             <p>
-                <label for='emailInput'>Email: </label>
-                <input id='emailInput' type='text' />
+                <label htmlFor='emailInput'>Email: </label>
+                <input id='emailInput' type='text' name='email' value={newUser.email} onChange={handleChange} />
             </p>
             <p>
-                <label for='tos'>Agree to Terms of Service</label>
-                <input id='tos' type='checkbox' />
+                <label htmlFor='tos'>Agree to Terms of Service</label>
+                <input id='tos' type='checkbox' name='acceptedTerms' checked={newUser.acceptedTerms} onChange={handleChange}/>
             </p>
             <button>Submit</button>
         </UserForm>
